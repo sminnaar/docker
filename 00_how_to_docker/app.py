51d084e111872eddac854afb069e511a3e8f1e20
sminnaar <sminnaar@student.wethinkcode.co.za>
2020-07-04 21:05:41 +0200

    More hello words

--- 00_how_to_docker/app.py
+++ 00_how_to_docker/app.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def msg():
-    return '<span><h1>Hello World</h1><span>'
+    return '<marquee><h1>Hello World</h1><h2>Hello World</h2><h3>Hello World</h3><h4>Hello World</h4><h5>Hello World</h5><h6>Hello World</h6><h7>Hello World</h7></marquee>'
