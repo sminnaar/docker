022a3495d04dc066c74e55aece02bfd7fe7003fd
sminnaar <sminnaar@student.wethinkcode.co.za>
2020-07-13 16:02:02 +0200

    Working on ex03

--- 00_how_to_docker/app.py
+++ 00_how_to_docker/app.py
@@ -4,3 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def msg():
     return '<marquee><h1>Hello World</h1><h2>Hello World</h2><h3>Hello World</h3><h4>Hello World</h4><h5>Hello World</h5><h6>Hello World</h6><h7>Hello World</h7></marquee>'
+
+@app.route('/hello')
+def msg():
+    return '<h1>Hello!<h1>'
+
+@app.route('/login')
+def msg():
+    return '<h1>yes<h1>'
+
+@app.route('/world')
+def msg():
+    return '<h1>Hello World!<h1>'
+
+@app.route('/item')
+def msg():
+    return '<h1>Hello Item!<h1>'
